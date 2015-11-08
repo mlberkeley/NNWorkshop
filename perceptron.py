47e07adcf2cfcf9d721513b1fa36a1670734c1d0
philkuz <superpker@gmail.com>
2015-11-07 17:16:15 -0800

    Created a decision class that wraps the Perceptron ideas into a readable format

--- perceptron.py
+++ perceptron.py
@@ -12,14 +12,73 @@ class Perceptron:
     1
     """
     def __init__(self, num_inputs, weights, threshold):
+        if num_inputs != len(weights):
+            raise ValueError('Input number and weight mismatch')
         self.ninputs = num_inputs
         self.weights = weights
         self.threshold = threshold
     def input(self, input_list):
+        if self.ninputs != len(input_list):
+            raise ValueError('The input list length should match the number of inputs')
         weighted_sum = np.dot(self.weights, input_list)
         if weighted_sum > self.threshold:
             return 1
         else:
             return 0
-p = Perceptron(3, [1,2,3], 4)
-print(p.input([0,1,1]))
+class Decision(Perceptron)
+    """ Models a binary decision that someone would want to make.
+
+    """
+    def __init__(self, decision, num_factors, weights, factors=[]):
+        Perceptron.__init__(self, num_factors, weights, num_factors*5)
+        self.decision = decision
+        self.factors = factors
+    def eval_factors(self):
+        print("Answer Y or N for the following questions")
+        factor_input = []
+        for i in range(self.ninputs):
+            valid_inp = False
+            while not valid_inp:
+                val = input(self.factors[i]+" ")
+                if val == "Y":
+                    valid_inp = True
+                    factors_input.append(1)
+                elif val == "N":
+                    valid_inp = True
+                    factors_input.append(0)
+                else:
+                    print("Invalid Input")
+        self.test_factors(factor_input)
+
+    def test_factors(self, factor_input):
+        value = Perceptron.input(self, factor_input)
+        if value:
+            print("You should", self.decision)
+        else:
+            print("You should not", self.decision)
+    def new_decision():
+        """ Starts a command line prompt that evaluates a new decision
+        """
+        decision = input('What would you like to decide today? ')
+        num_factors = int(input('How many factors does this depend on? '))
+        factors = []
+        weights = []
+        for i in range(1, factors+1):
+
+            factor = input('Factor {0}: '.format(i))
+            weight = int(input('How much does {0} matter to you (0-10)? '.format(factor)))
+            while weight > 10 or weight < 0:
+                weight = int(input(''{0} isn't a valid value, enter a number 0 - 10: '.format(weight)))
+            factors.append(factor)
+            weights.append(weight)
+        d = Decision(decision, num_factors, weights, factors)
+        print('Decision Created!')
+        keep_deciding = True
+        while keep_deciding:
+            command = input('Type "q" to quit or "f" to try new factors ')
+            if command == "q":
+                keep_deciding = False
+            elif command == "f":
+                d.eval_factors()
+            else:
+                print("Invalid command")
