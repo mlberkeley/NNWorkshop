76d3604ae4c9a7231a868c840903405b4c77f09b
William Guss <carlostnb@gmail.com>
2015-11-29 15:57:07 -0800

    Finished creation of neurons.

--- Workshop2/demo/network.py
+++ Workshop2/demo/network.py
@@ -5,14 +5,30 @@ the neural network workshop by Robotics @ Berkeley."""
 __author__ = "William Guss"
 
 import sigmoid
-
+from neuron import Neuron
+from connection import Connection
 
 class Network:
     """Represents a neural network using sigmoidal activations."""
 
-    def __init__(self, layers, activation=sigmoid.LogisticSigmoid):
+    def __init__(self, layers, activation=sigmoid.Logistic):
         """Constructs a neural network with a set of layers."""
-        self.activation = activation
+        self.bias = Neuron(sigmoid.Constant)
+
+        self.neurons = []
+        for layer in enumerate(layers):
+            self.neurons.append(list())
+            for i in range(0, layers[layer]):
+                # Input neurons shouldn't activate their input.
+                if layer is 0:
+                    self.neurons[layer].append(Neuron(sigmoid.LinearSigmoid))
+                else:
+                    self.neurons[layer].append(Neuron(activation))
+
+        self.connections = []
+
+
+
 
     def train(self, datapair, rate):
         """Trains the network with a certain learning rate on a datapair.
@@ -22,4 +38,9 @@ class Network:
         out = datapair[1]
 
         self.feedforward(inp)
-        error = self.backpropagate(out)
+        error = self.backpropagate(out, rate)
+
+        return error
+
+    def feedforward(self, input):
+
